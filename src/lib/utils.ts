69df0b0ba57719a1ef71ffd481ec644758ded6ca
Andreas Remdt <me@andreasremdt.com>
2024-06-19 21:46:17 +0200

    feat: properly merge classes, expand icon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(...classes));
+}
+
 export function createId(length: number = 5) {
   const CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
