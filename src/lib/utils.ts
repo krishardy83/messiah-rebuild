729a76809dc45b03ddc5c76651b3f2b0683d0b5d
Andreas Remdt <me@andreasremdt.com>
2024-09-05 20:09:34 +0200

    feat: add graduate program page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -36,3 +36,14 @@ export function getTime(date: Date) {
 export function slugify(str: string) {
   return str.toLowerCase().replace(/[\W_\s]+/g, "-");
 }
+
+export function isInViewport(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
