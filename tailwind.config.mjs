dd499aa16b2b995cdc54d7286741d6bf2a108036
krishardy83 <42982112+krishardy83@users.noreply.github.com>
2025-04-17 14:10:28 -0400

    Tailwind Safe List Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -58,7 +58,20 @@ export default {
     "bg-right",
     "bg-left",
     "w-1/4",
-    "w-1/3"
+    "w-1/4",
+    "w-1/4",
+    "w-1/3",
+    "w-2/3",
+    "lg:w-1/4",
+    "lg:w-1/4",
+    "lg:w-1/4",
+    "lg:w-1/3",
+    "lg:w-2/3",
+    "md:w-1/4",
+    "md:w-1/4",
+    "md:w-1/4",
+    "md:w-1/3",
+    "md:w-2/3"
   ],
   theme: {
     extend: {
