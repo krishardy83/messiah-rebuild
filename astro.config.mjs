162425760ba67c12cdbd974b9e06b7f7fa79b6b0
Andreas Remdt <me@andreasremdt.com>
2026-05-11 20:52:59 +0200

    chore: update astro to latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   integrations: [tailwind({ applyBaseStyles: false })],
   adapter: vercel(),
 });
